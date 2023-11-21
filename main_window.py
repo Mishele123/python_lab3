dd8c000e38ab3195b128cbcd3d5a867a29d2d8b6
Mishele123 <misha228665161@gmail.com>
2023-11-22 00:54:31 +0400

    Графический интерфейс приложения

--- main_window.py
+++ main_window.py
@@ -1,5 +1,5 @@
 import typing
-from PyQt6 import QtCore
+from PyQt6 import QtCore, QtWidgets
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import QFont
 import sys
@@ -10,21 +10,62 @@ from PyQt6.QtWidgets import QWidget
 class MainWindow(QWidget):
     def __init__(self) -> None:
         super().__init__()
-
+        self.resize(300, 100)
         self.setUi()
 
+    def create_label2(self) -> QLabel:
+        textbox1 = QLabel("Путь к файлу назначения:")
+        return textbox1
 
-    def create_textbox1(self) -> QLabel:
-        textbox1 = QLabel("Введите путь к папке исходного датасета:")
+    def create_label3(self) -> QLabel:
+        textbox1 = QLabel("Путь к папке назначения:")
         return textbox1
 
 
+    def create_textbox2(self) -> QPushButton:
+        textbox = QTextEdit()
+        textbox.setFixedHeight(27)
+        return textbox
+
+    def create_textbox3(self) -> QTextEdit:
+        textbox = QTextEdit()
+        textbox.setFixedHeight(27)
+        return textbox
+
+    def create_button1(self) -> QPushButton:
+        button = QPushButton("Создать файл аннотацию исходного датасета")
+        return button
+    
+    def create_button2(self) -> QPushButton:
+        button = QPushButton("создания датасета с другой организацией файлов ")
+        return button
+
+    def create_button3(self) -> QPushButton:
+        button = QPushButton("Следующая картинка tiger")
+        return button
+
+    def create_button4(self) -> QPushButton:
+        button = QPushButton("Следующая картинка leopard")
+        return button
+    
+
     def setUi(self) -> None:
         layout = QVBoxLayout()
-        
-        layout.addWidget(self.create_textbox1())
-
+        layout.addWidget(QLabel("Задание 1"))
+        layout.addWidget(self.create_label2())
+        layout.addWidget(self.create_textbox2())
+        layout.addWidget(self.create_button1())
+        layout.addWidget(QLabel("Задание 2"))
+        layout.addWidget(self.create_label3())
+        layout.addWidget(self.create_textbox3())
+        layout.addWidget(self.create_button2())
+        layout.addWidget(QLabel("Задание 3"))
+        layout.addWidget(self.create_button3())
+        layout.addWidget(self.create_button4())
         self.setLayout(layout)
+
+
+
 
 # def main() -> None:
 #     app = QApplication([])
